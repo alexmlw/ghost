7eb4d4c42a17c196ceeb9abb89702025217e4dfe
aitron <aitron@protonmail.com>
2019-05-15 16:04:05 +0300

    Add variable.sh

---
+++ variables.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+# Copy this file as variables.sh.
+# Here you can change settings for the build scripts.
+
+export AUTOCONF=autoconf2.64
+
+#export DIR_INSTALL=`pwd`
+export TOOLCHAIN_BASE=$DIR_INSTALL/ghost
+
+
+echo "$TOOLCHAIN_BASE"
